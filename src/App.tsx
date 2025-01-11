30ae2dac7dfe37fe838fb9eba821add18e555d93
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-01-12 00:52:18 +0100

    Added job container

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import { useEffect, useState } from 'react'
 import { Switch } from 'antd';
-
-
+import jobData from './data';
 import './App.css'
+
+interface JOb {
+  id: number,
+  company: string,
+  logo: string,
+  logoBackground: string,
+  position: string,
+  postedAt: string,
+  contract: string,
+  location: string,
+  website: string,
+  apply: string,
+  description: string,
+  requirements: {
+    contents: string,
+    items: [],
+  },
+  role: {
+    contents: string,
+    items: []
+  }
+
+
+
+}
 
 function App() {
 
 
   const [darkTheme, setDarkTheme] = useState(false)
+  const [jobs, setJobs] = useState<JOb[]>()
+
   const onChange = (checked: boolean) => {
     console.log(checked)
-  checked ? setDarkTheme(true) : setDarkTheme(false)
+    checked ? setDarkTheme(true) : setDarkTheme(false)
   };
 
   useEffect(() => {
-document.body.style.backgroundColor = darkTheme ? "#121721" : "#f2f2f2"
+    document.body.style.backgroundColor = darkTheme ? "#121721" : "#f2f2f2"
   }, [darkTheme])
+
+
+  // useEffect(() => {
+  //   console.log(jobData.map(job => job.company))
+  // }, [])
 
   return (
     <>
@@ -34,7 +65,7 @@ document.body.style.backgroundColor = darkTheme ? "#121721" : "#f2f2f2"
         <div className="header-filters">
           <input type="text" className={`search-input ${darkTheme ? "dark" : ""}`} placeholder='Filter by title, comnpanies, expertise...' />
           <input type="text" className={`location-filter ${darkTheme ? "dark" : ""}`} placeholder='Filter by location...' />
-          <div className={`job-hours-div ${darkTheme ? "dark": ""}`}>
+          <div className={`job-hours-div ${darkTheme ? "dark" : ""}`}>
             <div className="hours">
               <span className='span1'>
                 <span className='span2'>
@@ -46,8 +77,25 @@ document.body.style.backgroundColor = darkTheme ? "#121721" : "#f2f2f2"
           </div>
         </div>
       </header>
-      <main className={darkTheme ? "dark" : ""}>
-        
+
+      <main className={`jobs-list-container ${darkTheme ? "dark" : ""} `}>
+        {jobData.map((job) => {
+          return <div key={job.id} className='job-div'>
+            <div className="job-details">
+              <p>
+                <span>{job.postedAt}.</span>
+                <span>{job.contract}</span>
+              </p>
+              <h2>{job.position}</h2>
+              <p>{job.company}</p>
+              <p>{job.location}</p>
+            </div>
+            <div className='logo-container' style={{backgroundColor: job.logoBackground}}>
+              <img src={job.logo} alt="logo" />
+            </div>
+          </div>
+        })
+        }
       </main>
 
     </>
